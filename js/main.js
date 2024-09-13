543c15a75bf8bb4e34cee4b8c446a75e13a75c92
Stas Zhurakovskyi <stas290606@gmail.com>
2024-09-13 19:56:54 +0300

    Improve my adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navLinks = document.getElementById("nav-links")
+const burgerBTN = document.getElementById("burger-btn")
 
 function toogleMenu(){
     navLinks.classList.toggle("open")
+    burgerBTN.classList.toggle("is-open")
 }
